fbb1fd818cebd4fdb78fd5ce95f5a13cab8ab84c
emanabuwaked@gmail.com <emanabuwaked@gmail.com>
2020-05-18 02:06:34 +0300

    question number 1

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-Quize = {
-    question :"1- What is the capital of Palestine",
-    Aswer:{
-      a:  "Jerselem",
-      b:  "Nabules"
-    },
-    correct:"Jerselem"
 
-}
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var correct = 0;
+
+
+	if (question1 === "a sailboat") {
+		correct = correct+1;
+} 
+
+	
+
+
